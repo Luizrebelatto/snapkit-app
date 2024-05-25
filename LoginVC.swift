7b6d297e1d1acb14915172bcd67e7905e1c2649a
Luizrebelatto <lbrebelatto@gmail.com>
2024-05-24 21:34:53 -0300

    feat: create navigation

---
+++ LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  LoginVC.swift
+//  snapkitProject
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 24/05/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
--- snapkitProject/SceneDelegate.swift
+++ snapkitProject/SceneDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = ViewController()
-        window.rootViewController = vc
+        let vc: LoginVC = LoginVC()
+        let nav = UINavigationController(rootViewController: vc)
+        window.rootViewController = nav
         window.makeKeyAndVisible()
         self.window = window
     }
