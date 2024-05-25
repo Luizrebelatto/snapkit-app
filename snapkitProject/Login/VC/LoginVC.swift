b29ba365f66a31c421e98cff09352067dabe7b7e
Luizrebelatto <lbrebelatto@gmail.com>
2024-05-24 21:50:36 -0300

    feat: login screen

---
+++ snapkitProject/Login/VC/LoginVC.swift
@@ -0,0 +1,64 @@
+//
+//  LoginVC.swift
+//  snapkitProject
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi on 24/05/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    var loginScreen: LoginScreen?
+    
+    override func loadView() {
+        loginScreen = LoginScreen()
+        view = loginScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginScreen?.delegate(delegate: self)
+        loginScreen?.configTextFieldDelegate(delegate: self)
+    }
+
+
+}
+
+extension LoginVC: LoginScreenProtocol {
+    func tappedLoginButton() {
+        let vc: HomeVC = HomeVC()
+        let nav = UINavigationController(rootViewController: vc)
+        navigationController?.pushViewController(vc, animated: true)
+    }
+ 
+}
+
+extension LoginVC: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let email: String = loginScreen?.emailTextField.text ?? ""
+        let passwordTextField: String = loginScreen?.passwordTextField.text ?? ""
+        
+        if !email.isEmpty && !passwordTextField.isEmpty {
+            print("Botao habilitado")
+            loginScreen?.loginButton.isEnabled = true
+            loginScreen?.loginButton.backgroundColor = .darkGray
+        } else {
+            loginScreen?.loginButton.isEnabled = false
+            loginScreen?.loginButton.backgroundColor = .darkGray.withAlphaComponent(0.6)
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+}
+
